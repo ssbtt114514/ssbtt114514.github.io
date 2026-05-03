28b15f96c04861277be81c192489c9f87ff2e92d
ssbtt114514 <ssbtt114514@gmail.com>
2026-05-04 07:28:59 +0800

    创建 config/config.js

---
+++ config/config.js
@@ -0,0 +1,14 @@
+// 全局配置常量
+window.APP_CONFIG = {
+    QQ_NUMBER: '1973737092',
+    BILI_UID: '3546557150399113',
+    WECHAT_ID: 'ssbtt114514',
+    GITHUB_API_URL: 'https://api.github.com/users/ssbtt114514/repos?sort=updated&per_page=100',
+    GITHUB_HOMEPAGE: 'https://github.com/ssbtt114514',
+    QQ_ZONE_URL: 'https://user.qzone.qq.com/',
+    BILI_SPACE_URL: 'https://space.bilibili.com/',
+    BILI_USER_API: 'https://api.bilibili.com/x/space/acc/info?mid=3546557150399113',
+    CORS_PROXY: 'https://corsproxy.io/',
+    CONFIG_JSON_URL: './config.json',
+    LEVEL_EXP_TABLE: [0, 200, 1500, 4500, 10800, 28800, 0]
+};
