b59e02ecbc0e6c33d3bc465b28ae6690c3512192
ssbtt114514 <ssbtt114514@gmail.com>
2026-05-04 06:47:27 +0800

    更新 sprite.js

--- sprite.js
+++ sprite.js
@@ -8,7 +8,7 @@ const CONFIG = {
     QQ_ZONE_URL: 'https://user.qzone.qq.com/',
     BILI_SPACE_URL: 'https://space.bilibili.com/',
     BILI_USER_API: 'https://api.bilibili.com/x/space/acc/info?mid=3546557150399113',
-    CORS_PROXY: 'https://cors-anywhere.herokuapp.com/',
+    CORS_PROXY: 'https://corsproxy.io/',
     CONFIG_JSON_URL: './config.json'   // 著名项目配置文件
 };
 
